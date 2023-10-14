9ca7567ecf1702816f550af8eab839a3c57f8e62
Lufthansaaa <jay@jaythedev.com>
2023-10-13 21:16:52 -0700

    update features

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import random
 from roster import *
 from kickoff import kickoff
 
+
+
+
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -16,10 +19,10 @@ def play():
         time.sleep(0.5)
         clear_terminal()
         print("SET HUT!")
-        my_lineup.ball_holder = my_lineup.qb
         time.sleep(1)
         my_lineup = starting_lineup("Bartholomew Simpson", "Homer Griffin", "Bart Peterson", "Cricket Smith", "Moe Jones", "Bill Johnson", "Bartholomew Simpson")
         print(my_lineup.lineup)
+        my_lineup.ball_holder = my_lineup.qb
         clear_terminal()
         print(starting_lineup.lineup)
         play_type = input("Press R and ENTER to hand the ball off to the running back. Press Q and ENTER to do a passing play. Press V and ENTER to run with QB. ")
@@ -37,7 +40,15 @@ def play():
             defense_positions = ["CB", "NT", "REND", "LEND", "LOLB", "MIDLIB", "ROLB", "FS", "SS"]
             print("Trucked by " + random.choice(defense_positions))
             ball2 = abs(100 - yards_first)
-            print(f"The ball is now on the {ball2} yard line.")
+            if yards_first > 50:
+                yards_first = 100 - yards_first
+            ball2 = abs(100 - yards_first)
+            if ball2 >= 51:
+                ball3 = ball2 - 50
+                ball4 = 50 - ball3
+                print(f"The ball is now at the {ball4} yard line.")
+            else:
+                print(f"The ball is now at the {ball2} yard line.")
         elif play_type in ["q", "Q"]:
             passing_play = input("Press Z and ENTER to pass to Z, Press X and ENTER to pass to X, Press Y and ENTER to pass to Y.")
             if passing_play in ["z", "Z"]:
@@ -46,6 +57,12 @@ def play():
                 chance = random.randint(1, 4)
                 if chance == 1:
                     print("Intercepted by" + random.choice(defense_positions) + "!")
+                    intercept = random.randint(1, 2)
+                    match intercept:
+                        case 1:
+                            print("The defense now turns it over.")
+                        case 2:
+                            print("The offense causes a fumble, which gets the ball turned over to the offense.")
                 elif chance == 2:
                     print("The pass was incomplete, now going into second down.")
                     down = 2
@@ -57,6 +74,7 @@ def play():
                         print("The offense recovered the ball.")
                     elif fumble1 == 2:
                         print("THE DEFENSE TURNS IT OVER!")
+
 
     else:
         play_start = input("The other team has the ball, press enter to start the play. ")
